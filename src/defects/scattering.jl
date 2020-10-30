12ffbca32c67650cb88757fcab5477ca8a6091ab
Aleksandr Rodin <alerodin@gmail.com>
2020-10-30 14:18:37 +0800

    Bug fixes

--- src/defects/scattering.jl
+++ src/defects/scattering.jl
@@ -1,23 +1,27 @@
 include("defect_types.jl")
+include("../pristine/propagator.jl")
 
 # A function that takes a list of impurities and another one of directly
 # modified atoms and returns a list of all the scattering atoms
 function scattering_atoms(imps::Vector{ImpurityState}, mod_atoms::Vector{PerturbedAtom})
     # Start by obtaining the atoms that are coupled to the impurities
     # Get all the couplings from the impurity states
-    all_couplings = reduce(vcat, map(x -> x.coupling, imps))
+    all_couplings = collect(Iterators.flatten(map(x -> x.coupling, imps)))
+    # all_couplings = reduce(vcat, map(x -> x.coupling, imps))
     # all_couplings = collect(Iterators.flatten(map(x -> x.coupling, imps)))
     # Turn the couplings into PerturbedAtoms and get the unique hosts
     hosts = map(x -> PerturbedAtom(0.0, 0.0, x.coord), all_couplings) |> unique
     # Next, we get the neighbors of mod_atoms
-    all_neighbors = reduce(vcat, map(neighbors, map(x -> x.coord, mod_atoms)))
+    all_neighbors = collect(Iterators.flatten(map(neighbors, map(x -> x.coord, mod_atoms))))
+    # all_neighbors = reduce(vcat, map(neighbors, map(x -> x.coord, mod_atoms)))
     # We combine the hosts with the neighbors and get a unique list
     hosts_neighbors = vcat(hosts, all_neighbors) |> unique
     # If the same coordinate appears in hosts_neighbors and in mod_atoms, take
     # the PerturbedAtom from mod_atoms. First, identify the mod_atoms coords
     mod_coords = map(x -> x.coord, mod_atoms)
     # Next, keep only the host coordinates that do NOT appear in the mod_coords
-    hosts_neighbors = filter(x -> x.coord ∉ mod_coords, hosts_neighbors)
+    hosts_neighbors =
+        Vector{PerturbedAtom}(filter(x -> x.coord ∉ mod_coords, hosts_neighbors))
     # Now, we can combine the perturbed and host atoms
     all_perturbed_atoms = vcat(hosts_neighbors, mod_atoms)
     return all_perturbed_atoms
@@ -47,8 +51,7 @@ function Vj(scatterers::Vector{PerturbedAtom}, imp::ImpurityState)
     # ... and the coordinates of the graphene atoms that correspond to each V
     coords = map(x -> x.coord, couplings)
 
-    # return map(y -> sum((map(x -> y == x, coords)) .* Vs)::Float64, atom_coords)
-    return map(y -> (y .== coords) .* Vs::Vector{Float64}, atom_coords)
+    return map(y -> sum((map(x -> y == x, coords)) .* Vs)::Float64, atom_coords)
 end
 
 function V(scatterers::Vector{PerturbedAtom}, imps::Vector{ImpurityState})
